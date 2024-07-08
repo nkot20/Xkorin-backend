b146cf988be3bf75dac8cb6f80a365afc5dbfa79
nkot20 <74417927+nkot20@users.noreply.github.com>
2024-07-08 16:57:23 +0100

    print certificates

--- repositories/PersonRepository.js
+++ repositories/PersonRepository.js
@@ -1,5 +1,6 @@
 const Person = require('../models/Person');
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 class PersonRepository {
 
@@ -15,6 +16,15 @@ class PersonRepository {
     async findPersonByEmail(email){
         try {
             return await Person.findOne({email});
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async updatePerson(personId, data) {
+        try {
+            return await Person.findOneAndUpdate({_id: personId}, data, {new: true});
         } catch (error) {
             console.error(error);
             throw error;
